e9db2e0da6ea268c69f65e3642d0a2b31abf806c
Kunj9.github.io <82100365+Kunj9@users.noreply.github.com>
2024-11-08 11:55:42 -0500

    Create 006_insert_query_user_and_pet.sql

---
+++ SQL/006_insert_query_user_and_pet.sql
@@ -0,0 +1,5 @@
+INSERT INTO Users (email, password) 
+VALUES ('johndoe@example.com', 'hashed_password');
+
+INSERT INTO Pets (name, posted_by, breed, species, gender, found_datetime, lost_datetime, additional_details, status) 
+VALUES ('Buddy', 'johndoe', 'Golden Retriever', 'Dog', 'Male', '2024-11-01 10:00:00', NULL, 'Friendly and well-trained', 'Found');
